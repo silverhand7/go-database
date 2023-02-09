aff897bfe1815561b700af2d5fd1739eb517176c
silverhand7 <refojunior5@gmail.com>
2023-02-09 17:49:45 +0800

    learning golang datatype DB

--- database.go
+++ database.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go_database")
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/go_database?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
--- sql_test.go
+++ sql_test.go
@@ -2,8 +2,10 @@ package go_database
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -47,6 +49,81 @@ func TestQuerySql(t *testing.T) {
 		}
 		fmt.Println("id:", id)
 		fmt.Println("name:", name)
+	}
+
+	fmt.Println("Success")
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	query := `SELECT
+		id,
+		name,
+		email,
+		balance,
+		rating,
+		birth_date,
+		is_married,
+		created_at
+		FROM customers`
+
+	rows, err := db.QueryContext(ctx, query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close() // close when finish executing
+
+	// iterate all data
+	for rows.Next() {
+		var id int
+		var name string
+		var email sql.NullString
+		var balance sql.NullInt32
+		var rating sql.NullFloat64
+		var birthDate sql.NullTime
+		var isMarried sql.NullBool
+		var createdAt time.Time
+
+		err := rows.Scan(
+			&id,
+			&name,
+			&email,
+			&balance,
+			&rating,
+			&birthDate,
+			&isMarried,
+			&createdAt,
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("=================")
+		fmt.Println("id:", id)
+		fmt.Println("name:", name)
+		if email.Valid == true {
+			fmt.Println("email:", email.String)
+		}
+		if balance.Valid == true {
+			fmt.Println("balance:", balance.Int32)
+		}
+		if rating.Valid == true {
+			fmt.Println("rating:", rating.Float64)
+		}
+		if birthDate.Valid == true {
+			fmt.Println("birth date:", birthDate.Time)
+		}
+		if isMarried.Valid == true {
+			fmt.Println("is married:", isMarried.Bool)
+		}
+		fmt.Println("createdAt:", createdAt)
 	}
 
 	fmt.Println("Success")
